60cdfec31c33c31520ddfdae5d59dcb69291a313
MinhTri <vodaiminhtri@gmail.com>
2026-03-13 17:27:43 +0700

    chuẩn bị deloy

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,10 +3,12 @@ import tasksRoute from "./routes/tasksRoute.js";
 import { connectDB } from "./config/db.js";
 import dotevn from "dotenv";
 import cors from "cors";
+import path from "path";
 
 dotevn.config();
 
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 const app = express();
 //midware
@@ -14,6 +16,21 @@ app.use(express.json());
 app.use(cors({ origin: "http://localhost:5173" }));
 
 app.use("/api/tasks", tasksRoute);
+
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors({ origin: "http://localhost:5173" }));
+}
+
+if (process.env.NODE_ENV === "production") {
+  // Dùng ".." để nhảy ra khỏi thư mục backend
+  const frontendPath = path.join(__dirname, "..", "frontend", "dist");
+
+  app.use(express.static(frontendPath));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(frontendPath, "index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
